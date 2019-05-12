17c94c66bd114301690957a9e2aee7c9a887a0c9
Eric Stephenson <ericb.stephenson@gmail.com>
2019-05-12 14:08:36 -0700

    Add react navbar pack

---
+++ app/javascript/navbar/App.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/">Twain</NavbarBrand>
+
+        <NavbarToggler onClick={this.toggle} />
+
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/projects">Projects</NavLink>
+            </NavItem>
+
+            <NavItem>
+              <NavLink href="/articles">Articles</NavLink>
+            </NavItem>
+
+            <NavItem>
+              <NavLink href="/labs">Labs</NavLink>
+            </NavItem>
+
+            <NavItem>
+              <NavLink href="/skills">Skills</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
+}
