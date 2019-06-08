ee52a75de5dce5ba0b921dd947b4e0672b5fee5f
Eric Stephenson <ericb.stephenson@gmail.com>
2019-06-08 10:18:02 -0700

    Update projects table, Edit projects seed yaml

--- db/migrate/20190512174046_create_projects.rb
+++ db/migrate/20190512174046_create_projects.rb
@@ -1,17 +1,18 @@
 class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
+      t.string   :title
       t.string   :slug
       t.string   :cover
       t.string   :images, array: true, default: []
       t.string   :meta_title
       t.string   :meta_description
-      t.string   :title
       t.text     :summary
       t.string   :site_link
       t.string   :repo_link
       t.text     :description, array: true, default: []
-      t.string   :highlights, array: true, default: []
+      t.string   :features, array: true, default: []
+      t.string   :apis, array: true, default: []
       t.string   :tech_stack, array: true, default: []
 
       t.timestamps
